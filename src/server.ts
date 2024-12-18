c20b34e98a410254bbd235e6aa213d15c3c285bb
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-18 19:46:19 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
-
+import { env } from "./env";
 app.listen({
-    port: 3333,
+    port: env.PORT,
     host: '0.0.0.0'
 }).then(() => {
     console.log('🚀 HTTP Server Running!');
