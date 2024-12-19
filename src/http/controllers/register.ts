e0045ad254b0f526c9f46fa1ddd8582d7a18affd
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-19 15:36:21 -0300

    Create user

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,25 +1,36 @@
-
 import { FastifyRequest, FastifyReply } from "fastify";
-import {prisma} from "@/lib/prisma";
-import {z} from "zod";
-
+import { hash } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
-    });
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-    const { name, email, password } = registerBodySchema.parse(request.body)
-    
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password,
-        }
-    })
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
-    return replay.status(201).send()
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return replay.status(409).send()
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+
+  return replay.status(201).send();
 }
