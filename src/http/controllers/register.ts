f95f5a347d2a60a0709058495417bd147522c830
Luiz Chiaradia <luizexchiaradia@gmail.com>
2024-12-20 08:22:04 -0300

    up

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { RegisterUseCase } from "@/use-cases/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, replay: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,12 +14,18 @@ export async function register(request: FastifyRequest, replay: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
+
     const usersRepository = new PrismaUsersRepository();
     const registerUseCase = new RegisterUseCase(usersRepository);
-    console.log(registerUseCase)
+
     await registerUseCase.execute({ name, email, password });
+
   } catch (err) {
-    return replay.status(409).send();
+    if (err instanceof UserAlreadyExistsError) {
+      return replay.status(409).send({message: err.message});
+    }
+    return replay.status(500).send(); // TODO fix this
   }
+
   return replay.status(201).send();
 }
