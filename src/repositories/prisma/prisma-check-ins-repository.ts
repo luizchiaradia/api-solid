eaaf25288d44ad97a4f4b0e6472fdc92dc900ad8
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-01-13 09:23:12 -0300

    prisma repositories

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,8 +1,44 @@
 import { prisma } from "@/lib/prisma";
 import { Prisma, CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
+import dayjs from "dayjs";
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
+    return checkIns;
+  }
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({
+      where: {
+        user_id: userId,
+      },
+    });
+    return count;
+  }
+  async findById(checkInId: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id: checkInId,
+      },
+    });
+    return checkIn;
+  }
+  async save(data: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    });
+    return checkIn;
+  }
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = await prisma.checkIn.create({
       data,
@@ -11,15 +47,19 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
   }
 
   async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
     const checkIn = await prisma.checkIn.findFirst({
       where: {
         user_id: userId,
         created_at: {
-          gte: date,
-          lt: new Date(date.getTime() + 24 * 60 * 60 * 1000),
+          gte: startOfTheDay.toDate(),
+          lt: endOfTheDay.toDate(),
         },
       },
     });
     return checkIn;
   }
 }
+
+
--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,13 +1,35 @@
-import { prisma } from "@/lib/prisma"
-import { Prisma, Gym } from "@prisma/client"
-import { GymRepository } from "../gyms-repository"
+import { prisma } from "@/lib/prisma";
+import { Prisma, Gym } from "@prisma/client";
+import { FindManyNearbyParams, GymRepository } from "../gyms-repository";
 
 export class PrismaGymsRepository implements GymRepository {
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: "insensitive",
+        },
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
+    return gyms;
+  }
+
+  async findManyNearby({latitude, longitude}: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * FROM gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `;
+    return gyms;
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym = await prisma.gym.create({
       data,
-    })
-    return gym
+    });
+    return gym;
   }
 
   async findById(id: string) {
@@ -15,7 +37,7 @@ export class PrismaGymsRepository implements GymRepository {
       where: {
         id,
       },
-    })
-    return gym
+    });
+    return gym;
   }
 }
