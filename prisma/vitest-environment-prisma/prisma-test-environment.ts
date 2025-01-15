fcaffca4b7214ab4100c3245edfa1173e0c3cb7e
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-01-15 10:02:15 -0300

    register prisma test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,32 @@
+import { randomUUID } from 'node:crypto';
 import type { Environment } from 'vitest'
+import "dotenv/config";
+import { execSync } from 'node:child_process';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if(!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("Setting up Prisma test environment");
+    const schema = randomUUID();
+    const DATABASE_URL = generateDatabaseURL(schema);
+    process.env.DATABASE_URL = DATABASE_URL;
+    execSync ('npx prisma migrate deploy');
     return {
       async teardown() {
-        console.log("Tearing down Prisma test environment");
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+        await prisma.$disconnect();
       },
     };
   },
