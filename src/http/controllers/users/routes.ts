a0b1fc738877fecf3d5bfb0f99ab094b7a6a76f2
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-01-20 12:59:25 -0300

    routes

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import { register } from "@/http/controllers/users/register";
+import { authenticate } from "@/http/controllers/users/authenticate";
+import { profile } from "@/http/controllers/users/profile";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
+
+export async function usersRoutes(app:FastifyInstance) {
+    app.post("/users", register)
+    app.post("/sessions", authenticate)
+    app.get("/me", {onRequest: [verifyJWT]} ,profile)
+}
