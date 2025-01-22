0bd547d1f3ac45a73b3681dc92e523a3ee83e78a
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-01-22 13:49:54 -0300

    Update in memory users repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,5 @@
 import { UsersRepository } from '@/repositories/users-repository'
-import { User, Prisma } from '@prisma/client'
+import { User, Prisma, Role } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
@@ -25,6 +25,7 @@ export class InMemoryUsersRepository implements UsersRepository {
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date(),
+      role: "MEMBER" as Role,
     }
     this.items.push(user)
     return user
